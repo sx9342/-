4cb2fe96f22557a42d639e3357d9bfac188c5dc4
LIN BO <rin_laurant@archwsl.com>
2024-11-13 17:06:01 +0800

    删除多余文件

---
+++ src/modify_score.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "head.h"
+void modify_score()
+{
+    char name[20];
+    int id;
+    printf("请进行姓名学号进行双重认证：\n");
+    scanf("%s", name);
+    scanf("%d", &id);
+    Node *p = g_head;
+    
+    
+}
